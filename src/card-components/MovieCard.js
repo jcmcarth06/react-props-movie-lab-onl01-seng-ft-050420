7214a9808e91022fcd686a0f0e3ae5432fdaf5dd
Joshua McArthur <jcmcarth06@gmail.com>
2020-09-07 21:23:03 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -30,7 +30,7 @@ const posterMap = {
 export default class MovieCard extends Component {
 
   render() {
-    console.log(this.props)
+    console.log(this.props);
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
